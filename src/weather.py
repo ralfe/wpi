d03b78f6c305b59e83b3445e304b40e06ee4a697
Ralf Enderle <ralf.enderle@exxcellent.de>
2015-02-01 12:15:33 +0100

    first draft of weather data call

---
+++ src/weather.py
@@ -0,0 +1,34 @@
+__author__ = 'renderle'
+
+import requests
+import json
+
+print("start parsing weather data")
+
+print("----> 1: do request")
+
+url = 'http://api.openweathermap.org/data/2.5/weather'
+data = 'q=ulm&units=metric'
+response = requests.get(url, params=data)
+
+print()
+
+print("----> 2: WeatherData response")
+
+print("URL :", response.url)
+print("status code: ", response.status_code)
+print("data ", response.json())
+
+print()
+
+print("----> 3: parsing json")
+
+data = response.json()
+
+print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+
+
+
+
+
+
